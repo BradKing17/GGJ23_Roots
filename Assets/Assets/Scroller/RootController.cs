82250ed4dc1aee0acdaf4363f6f8ef7328a79b18
Brad King <king.brad1997@gmail.com>
2023-02-04 20:42:22 +0000

    Point Dropping added - will be changed to vector 3's

--- Assets/Assets/Scroller/RootController.cs
+++ Assets/Assets/Scroller/RootController.cs
@@ -11,30 +11,56 @@ public class RootController : MonoBehaviour
     public int turnSpeed = 5;
 
 
-    List<Vector2> points;
+    List<GameObject> points;
     public GameObject guide;
+    public float pointTimer = 0.2f;
+    float curPointTimer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        curPointTimer = pointTimer;
        // joystickInput = new Vector2(0, 0);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (isGrowing)
         {
+            //Movement
             float hAxis = -Input.GetAxis("Horizontal") * -turnSpeed * Time.deltaTime;
 
             transform.Rotate(0, hAxis, 0);
             this.transform.Translate(this.transform.forward * speed * Time.deltaTime);
+
+            
+            //Point Dropping
+            if(curPointTimer >= 0.0f)
+            {
+                curPointTimer -= Time.deltaTime;
+            }
+            else
+            {
+               
+                dropPoint();
+                
+
+            }
         }
     }
 
     void dropPoint()
     {
+        curPointTimer = pointTimer;
+        Debug.Log("poop");
+        Debug.Log(curPointTimer);
 
+        var clone = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        clone.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+        clone.transform.position = this.transform.position;
+        Physics.IgnoreCollision(clone.GetComponent<Collider>(), GetComponent<Collider>());
     }
+
+
 }
