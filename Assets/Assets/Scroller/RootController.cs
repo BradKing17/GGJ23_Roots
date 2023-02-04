6f64ff868c7d3629605631620288a46efc5dbb85
Brad King <king.brad1997@gmail.com>
2023-02-04 20:10:27 +0000

    Simple Root head movement, quaternions are a ball ache

--- Assets/Assets/Scroller/RootController.cs
+++ Assets/Assets/Scroller/RootController.cs
@@ -6,21 +6,31 @@ public class RootController : MonoBehaviour
 {
     bool isGrowing = true;
 
-    int speed = 5;
-    int turnSpeed = 5;
-    Vector2 direction;
+    
+    public int speed = 5;
+    public int turnSpeed = 5;
 
+
+    List<Vector2> points;
+    public GameObject guide;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
+       // joystickInput = new Vector2(0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isGrowing)
+        {
+            float hAxis = -Input.GetAxis("Horizontal") * -turnSpeed * Time.deltaTime;
+
+            transform.Rotate(0, hAxis, 0);
+            this.transform.Translate(this.transform.forward * speed * Time.deltaTime);
+        }
     }
 
     void dropPoint()
