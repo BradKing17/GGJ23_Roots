cf5d9843f0adabd474b665cec72f3e8efa01759d
Brad King <king.brad1997@gmail.com>
2023-02-04 17:50:09 +0000

    Removed AirConsole plugin

---
+++ Assets/Assets/Scroller/RootController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RootController : MonoBehaviour
+{
+    bool isGrowing = true;
+
+    int speed = 5;
+    int turnSpeed = 5;
+    Vector2 direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void dropPoint()
+    {
+
+    }
+}
