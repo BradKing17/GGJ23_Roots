e4a372d4787db4dbe8e8bf7d67bef7ef1b35814c
Brad King <king.brad1997@gmail.com>
2023-02-05 13:09:22 +0000

    Item Spawning and effects added

--- Assets/Assets/Scroller/RootController.cs
+++ Assets/Assets/Scroller/RootController.cs
@@ -8,11 +8,13 @@ public class RootController : MonoBehaviour
     public bool isGrowing = true;
     public RootPlayerActions playerActions;
 
-    public int minSpeed = 2;
-    public int maxSpeed = 5;
-    public int turnSpeed = 5;
 
-    public int curSpeed = 4;
+    public float avgSpeed = 3;
+    public float curSpeed = 4;
+
+    public float curTurnSpeed = 5; 
+    public float avgTurnSpeed = 5;
+
 
     private Vector2 input;
 
@@ -30,7 +32,6 @@ public class RootController : MonoBehaviour
     {
         playerActions = new RootPlayerActions();
         playerActions.Enable();
-        curPointTimer = pointTimer;
         var gamepad = Gamepad.current;
 
         // joystickInput = new Vector2(0, 0);
@@ -54,13 +55,58 @@ public class RootController : MonoBehaviour
 
             this.transform.Translate(this.gameObject.transform.forward * curSpeed * Time.deltaTime);
 
-                float hAxis = input.x * turnSpeed * Time.deltaTime;
-                transform.Rotate(0, hAxis, 0);
+                float hAxis = input.x * curTurnSpeed * Time.deltaTime;
+                transform.Rotate(0, hAxis, 0, Space.Self);
 
             
         }
     }
 
     public void OnMove(InputAction.CallbackContext context) => input = context.ReadValue<Vector2>();
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Obstacle"))
+        {
+            Death();
+        }
+        isPoweredUp = true;
+        StartCoroutine(PowerTimer());
+        if (isPoweredUp)
+        {
+            switch (other.tag)
+            {
+                case "Fertilizer":
+                    curSpeed = curSpeed * 2.0f;
+                    curTurnSpeed = curTurnSpeed * 2.0f;
+                    break;
+
+                case "Water":
+                    break;
+
+                case "Toxic":
+                    curSpeed = curSpeed * 0.5f;
+                    curTurnSpeed = curTurnSpeed * 0.5f;
+                    break;
+
+            }
+        }
+    }
+
+    void Death()
+    {
+        OnDisable();
+        isGrowing = false;
+    }
+
+    IEnumerator PowerTimer()
+    {
+
+        yield return new WaitForSeconds(powerUpTimer);
+        isPoweredUp = false;
+        curSpeed = avgSpeed;
+        curTurnSpeed = avgTurnSpeed;
+
+    }
 
 }
--- Assets/Assets/Scroller/SpawnController.cs
+++ Assets/Assets/Scroller/SpawnController.cs
@@ -9,12 +9,17 @@ public class SpawnController : MonoBehaviour
     public GameObject rootObj;
     public List<RootController> players;
     public List<GameObject> spawnPoints;
+    public List<GameObject> objectSpawns;
+
+    public List<GameObject> items;
+    public bool gameRunning = false;
+    public float objTimer = 4;
 
     // Start is called before the first frame update
     void Start()
     {
         noOfPlayers = Gamepad.all.Count;
-        for(int i = 0; i < noOfPlayers; i++)
+        for (int i = 0; i < noOfPlayers; i++)
         {
             var clone = PlayerInput.Instantiate(rootObj, playerIndex: i, pairWithDevice: Gamepad.all[i]);
             players.Add(clone.GetComponent<RootController>());
@@ -30,7 +35,10 @@ public class SpawnController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(gameRunning)
+        {
+            SpawnItems();
+        }
     }
 
     IEnumerator StartCountDown()
@@ -41,10 +49,32 @@ public class SpawnController : MonoBehaviour
 
     void StartGame()
     {
-        for(int i= 0; i <players.Count; i++)
+        for (int i = 0; i < players.Count; i++)
         {
             players[i].isGrowing = true;
         }
+        gameRunning = true;
+        objTimer = 4;
     }
-     
+
+    void SpawnItems()
+    {
+        Debug.Log(objTimer);
+        
+        if(objTimer <= 0.0f)
+        {
+            int spawnPoint = Random.Range(0, objectSpawns.Count);
+            if (objectSpawns[spawnPoint].transform.childCount == 0)
+            {
+                int obj = Random.Range(0, items.Count);
+                Debug.Log(spawnPoint);
+                var clone = Instantiate(items[obj], objectSpawns[spawnPoint].transform);
+                objTimer = 4.0f;
+            }
+        }
+        else
+        {
+            objTimer -= Time.deltaTime;
+        }
+    }
 }
