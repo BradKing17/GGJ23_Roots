fa842f3dbb96310ecb29338f1df1fcdbf38c21b2
Matt <matt.j.bowes@gmail.com>
2023-02-05 00:13:47 +0000

    Mesh Generation Controller
    Script to create an easier path to mesh gen

--- Assets/Assets/Scroller/RootController.cs
+++ Assets/Assets/Scroller/RootController.cs
@@ -14,8 +14,8 @@ public class RootController : MonoBehaviour
 
     List<GameObject> points;
     public GameObject guide;
-    public float pointTimer = 0.2f;
-    float curPointTimer = 0.0f;
+    
+    
 
     float powerUpTimer = 5.0f;
     bool isPoweredUp = false;
@@ -23,7 +23,7 @@ public class RootController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        curPointTimer = pointTimer;
+        
         // joystickInput = new Vector2(0, 0);
     }
 
@@ -40,31 +40,10 @@ public class RootController : MonoBehaviour
             this.transform.Translate(this.transform.forward * curSpeed * Time.deltaTime);
 
 
-            //Point Dropping
-            if (curPointTimer >= 0.0f)
-            {
-                curPointTimer -= Time.deltaTime;
-            }
-            else
-            {
-
-                DropPoint();
-
-
-            }
+            
         }
     }
 
-    void DropPoint()
-    {
-        curPointTimer = pointTimer;
-        Debug.Log("poop");
-        Debug.Log(curPointTimer);
-
-        var clone = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        clone.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
-        clone.transform.position = this.transform.position;
-        Physics.IgnoreCollision(clone.GetComponent<Collider>(), GetComponent<Collider>());
-    }
+    
 
 }
